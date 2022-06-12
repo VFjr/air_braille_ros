e0f58a756ce9a11b70d3a8e0c5ed7cd08354ac2f
VFjr <victor.florea.jr@gmail.com>
2022-06-12 16:40:38 +0100

    added space between characters in text_page mode, added audio cues

--- scripts/air_braille_server.py
+++ scripts/air_braille_server.py
@@ -10,6 +10,32 @@ import tf2_geometry_msgs
 from geometry_msgs.msg import Pose, PoseArray, TransformStamped, PoseStamped
 from align_plane_to_points.srv import *
 
+import pyttsx3
+import threading
+import queue
+
+# To play audio text-to-speech during execution
+class TTSThread(threading.Thread):
+    def __init__(self, queue):
+        threading.Thread.__init__(self)
+        self.queue = queue
+        self.daemon = True
+        self.start()
+
+    def run(self):
+        tts_engine = pyttsx3.init()
+        tts_engine.startLoop(False)
+        t_running = True
+        while t_running:
+            if self.queue.empty():
+                tts_engine.iterate()
+            else:
+                data = self.queue.get()
+                if data == "exit":
+                    t_running = False
+                else:
+                    tts_engine.say(data)
+        tts_engine.endLoop()
 
 class AirBrailleServer:
     def __init__(self):
@@ -20,12 +46,28 @@ class AirBrailleServer:
         self.width = rospy.get_param('~width')
         self.height = rospy.get_param('~height')
 
+        self.pin_1_offset = rospy.get_param('~pin_1_offset')
+        self.pin_2_offset = rospy.get_param('~pin_2_offset')
+        self.pin_3_offset = rospy.get_param('~pin_3_offset')
+        self.pin_4_offset = rospy.get_param('~pin_4_offset')
+        self.pin_5_offset = rospy.get_param('~pin_5_offset')
+        self.pin_6_offset = rospy.get_param('~pin_6_offset')
+
         self.world_frame =str(rospy.get_param('~world_frame'))
         self.device_frame = str(rospy.get_param('~device_frame'))
 
         self.pixels_per_char = rospy.get_param('~pixels_per_char')
+        self.horizontal_spacer_pixels = rospy.get_param('~horizontal_spacer_pixels')
+        self.vertical_spacer_pixels = rospy.get_param('~vertical_spacer_pixels')
 
-        self.img = np.zeros((self.pixels_per_char*self.height,self.pixels_per_char*self.width,3), dtype=np.uint8)
+        self.sound_enabled = rospy.get_param('~sound_enabled')
+
+        self.mode = rospy.get_param('~mode') #single_char, text_page, image
+
+        self.total_height_in_pixels = self.pixels_per_char*self.height+self.vertical_spacer_pixels*(self.height-1)
+        self.total_width_in_pixels = self.pixels_per_char*self.width+self.horizontal_spacer_pixels*(self.width-1)
+
+        self.img = np.zeros((self.total_height_in_pixels,self.total_width_in_pixels,3), dtype=np.uint8)
         self.img.fill(255)
 
         self.serial_data_out_topic = str(rospy.get_param('~serial_data_out_topic'))
@@ -50,45 +92,45 @@ class AirBrailleServer:
         self.braille_dict = { " ": 0b00000000,
                         "a": 0b00000001,
                         "b": 0b00000011,
-                        "c": 0b00100001,
-                        "d": 0b00110001,
+                        "c": 0b00001001,
+                        "d": 0b00011001,
                         "e": 0b00010001,
-                        "f": 0b00100011,
-                        "g": 0b00110011,
+                        "f": 0b00001011,
+                        "g": 0b00011011,
                         "h": 0b00010011,
-                        "i": 0b00100010,
-                        "j": 0b00110010,
+                        "i": 0b00001010,
+                        "j": 0b00011010,
                         "k": 0b00000101,
                         "l": 0b00000111,
-                        "m": 0b00100101,
-                        "n": 0b00110101,
+                        "m": 0b00001101,
+                        "n": 0b00011101,
                         "o": 0b00010101,
-                        "p": 0b00100111,
-                        "q": 0b00110111,
+                        "p": 0b00001111,
+                        "q": 0b00011111,
                         "r": 0b00010111,
-                        "s": 0b00100110,
-                        "t": 0b00110110,
-                        "u": 0b00001101,
-                        "v": 0b00001111,
+                        "s": 0b00001110,
+                        "t": 0b00011110,
+                        "u": 0b00100101,
+                        "v": 0b00100111,
                         "w": 0b00111010,
                         "x": 0b00101101,
                         "y": 0b00111101,
-                        "z": 0b00011101,
+                        "z": 0b00110101,
                         "#": 0b00111100,
                         "1": 0b00000001,
                         "2": 0b00000011,
-                        "3": 0b00100001,
-                        "4": 0b00110001,
+                        "3": 0b00001001,
+                        "4": 0b00011001,
                         "5": 0b00010001,
-                        "6": 0b00100011,
-                        "7": 0b00110011,
+                        "6": 0b00001011,
+                        "7": 0b00011011,
                         "8": 0b00010011,
-                        "9": 0b00100010,
-                        "0": 0b00110010,
-                        ".": 0b00011010,
+                        "9": 0b00001010,
+                        "0": 0b00011010,
+                        ".": 0b00110010,
                         ",": 0b00000010,
                         "!": 0b00010110,
-                        "?": 0b00001110,
+                        "?": 0b00100110,
         }
 
     def load_text_file(self):
@@ -104,51 +146,52 @@ class AirBrailleServer:
         f.close()
 
     def draw_text_page(self):
-        #draw vertical lines
+        #draw vertical rectangles
 
-        for i in range(self.width+1):
-            point_top = (self.pixels_per_char*i-1, 0)
-            point_bottom = (self.pixels_per_char*i-1, self.pixels_per_char*self.height)
-            self.img = cv2.line(self.img,point_top, point_bottom, 0,2)
+        for i in range(self.width-1):
+            point_start = (self.pixels_per_char*(i+1) + self.horizontal_spacer_pixels * i, 0)
+            point_end = (self.pixels_per_char*(i+1)+self.horizontal_spacer_pixels * (i+1), self.total_height_in_pixels)
+            self.img = cv2.rectangle(self.img, point_start, point_end, (200,200,200), -1)
 
-        #draw horizontal lines
-        for i in range(self.height+1):
-            point_left = (0, self.pixels_per_char*i-1)
-            point_right = (self.width*self.pixels_per_char, self.pixels_per_char*i-1)
-            self.img = cv2.line(self.img,point_left, point_right, 0,2)
+        #draw horizontal rectangles
 
+        for i in range(self.height-1):
+            point_start = (0, self.pixels_per_char*(i+1) + self.vertical_spacer_pixels * i)
+            point_end = (self.total_width_in_pixels, self.pixels_per_char*(i+1)+self.vertical_spacer_pixels * (i+1))
+            self.img = cv2.rectangle(self.img, point_start, point_end, (150,150,150), -1)
+
+        #draw text
         for iy,ix in np.ndindex(self.ascii_page.shape):
 
             #origin = (ix*pixels_per_char+10,iy*pixels_per_char+30)
             character = chr(self.ascii_page[iy,ix])
 
             textsize = cv2.getTextSize(character, cv2.FONT_HERSHEY_SIMPLEX, 1, 2)[0]
-            textX = int((self.pixels_per_char-textsize[0])/2 + ix*self.pixels_per_char)
-            textY = int((self.pixels_per_char + textsize[1])/2 + iy*self.pixels_per_char)
+            textX = int((self.pixels_per_char-textsize[0])/2 + ix*self.pixels_per_char + ix * self.horizontal_spacer_pixels)
+            textY = int((self.pixels_per_char + textsize[1])/2 + iy*self.pixels_per_char + iy*self.vertical_spacer_pixels)
 
             self.img = cv2.putText(self.img, character,(textX,textY), cv2.FONT_HERSHEY_SIMPLEX,1,0,2,cv2.LINE_AA)
+
     
-    def draw_cursor(self, x_char_id, y_char_id):
+    def draw_cursor(self, x_coord_int, y_coord_int):
 
         #cursor_img = np.zeros((self.pixels_per_char*self.height,self.pixels_per_char*self.width,3), dtype=np.uint8)
         #cursor_img.fill(255)
         cursor_img = self.img.copy()
 
-        x_coord = int(x_char_id*self.pixels_per_char)
-        y_coord = int(y_char_id*self.pixels_per_char)
 
-        if x_coord < 0:
-            x_coord = 0
-        if x_coord >= (self.width*self.pixels_per_char):
-            x_coord = (self.width*self.pixels_per_char)
+        if x_coord_int < 0:
+            x_coord_int = 0
+        if x_coord_int >= self.total_width_in_pixels:
+            x_coord_int = self.total_width_in_pixels-1
 
-        if y_coord < 0:
-            y_coord = 0
-        if y_coord >= (self.height*self.pixels_per_char):
-            y_coord = (self.height*self.pixels_per_char)
+        if y_coord_int < 0:
+            y_coord_int = 0
+        if y_coord_int >= self.total_height_in_pixels:
+            y_coord_int = self.total_height_in_pixels-1
 
-        cursor_img = cv2.circle(cursor_img, (x_coord,y_coord), radius = 3, color = (0,0,255), thickness = 2)
-        cursor_img = cv2.circle(cursor_img, (x_coord,y_coord), radius = 15, color = (0,0,255), thickness = 2)
+        cursor_img = cv2.circle(cursor_img, (x_coord_int,y_coord_int), radius = 4, color = (0,0,255), thickness = 2)
+        cursor_img = cv2.circle(cursor_img, (x_coord_int,y_coord_int), radius = 15, color = (0,0,255), thickness = 2)
 
         return cursor_img
 
@@ -156,8 +199,22 @@ class AirBrailleServer:
         
         return self.braille_dict.get(character,0b00000000)
 
+    def pixel_coord_to_id_and_spacer(self,x_coord, y_coord):
 
+        is_on_horizontal_spacer = False
+        is_on_vertical_spacer = False
 
+        x_id = int(x_coord / (self.pixels_per_char + self.horizontal_spacer_pixels))
+
+        if (x_coord % (self.pixels_per_char + self.horizontal_spacer_pixels)) >= self.pixels_per_char:
+            is_on_horizontal_spacer = True
+        
+        y_id = int(y_coord / (self.pixels_per_char + self.vertical_spacer_pixels))
+
+        if (y_coord % (self.pixels_per_char + self.vertical_spacer_pixels)) >= self.pixels_per_char:
+            is_on_vertical_spacer = True
+
+        return x_id, y_id, is_on_horizontal_spacer, is_on_vertical_spacer
 
 
 
@@ -167,6 +224,9 @@ if __name__ == '__main__':
     rospy.init_node('air_braille_server')
 
     server = AirBrailleServer()
+
+    q = queue.Queue() 
+    tts_thread = TTSThread(q)
    
     tfBuffer = tf2_ros.Buffer()
     listener = tf2_ros.TransformListener(tfBuffer)
@@ -174,7 +234,59 @@ if __name__ == '__main__':
 
     publisher = rospy.Publisher(server.serial_data_out_topic , UInt8, queue_size=10)
 
+    rospy.loginfo("Starting Display Calibration")
+    rospy.loginfo("Warm Up Procedure")
 
+    publisher.publish(0b00000000)
+    rospy.sleep(0.3)
+    publisher.publish(0b00111111)
+    rospy.sleep(0.3)
+    publisher.publish(0b00000000)
+    rospy.sleep(0.3)
+    publisher.publish(0b00111111)
+    rospy.sleep(0.3)
+
+    rospy.loginfo("Moving All Pins Down")
+
+    publisher.publish(0b00000000)
+
+    rospy.sleep(0.3)
+    r = rospy.Rate(10)
+
+    for _ in range(10):
+        #Sforce move pins down
+        publisher.publish(0b10000000)
+        r.sleep()
+
+    rospy.loginfo("Moving Pins Up by Calibrated Distances")
+
+    for _ in range(server.pin_1_offset):
+        publisher.publish(0b10011001)
+        r.sleep()
+    
+    for _ in range(server.pin_2_offset):
+        publisher.publish(0b10011010)
+        r.sleep()
+
+    for _ in range(server.pin_3_offset):
+        publisher.publish(0b10011011)
+        r.sleep()
+
+    for _ in range(server.pin_4_offset):
+        publisher.publish(0b10011100)
+        r.sleep()
+
+    for _ in range(server.pin_5_offset):
+        publisher.publish(0b10011101)
+        r.sleep()
+
+    for _ in range(server.pin_6_offset):
+        publisher.publish(0b10011110)
+        r.sleep()
+
+    publisher.publish(0b00111111)
+
+    rospy.loginfo("Finished Display Calibration")
 
     #get position of point 1
     rospy.loginfo("waiting for first point")
@@ -225,8 +337,6 @@ if __name__ == '__main__':
     trans3.header.frame_id = server.world_frame
     trans3.child_frame_id = "point3"
    
-    
-
     #use the service to get the aligned point
 
     rospy.wait_for_service('align_plane')
@@ -242,81 +352,70 @@ if __name__ == '__main__':
     except rospy.ServiceException as e:
         rospy.logerr("Service call failed: %s"%e)
 
-
-    #print("trans1")
-    #print(trans1)
-
-    #print("response")
-    #print(resp)
-
     trans_aligned = TransformStamped()
     trans_aligned.transform.translation = resp.alignedpose.position
     trans_aligned.transform.rotation = resp.alignedpose.orientation
     trans_aligned.header.frame_id = server.world_frame
     trans_aligned.child_frame_id = "aligned_point"
 
-
-    #print("trans_aligned")
-    #print(trans_aligned)
-
-    #print(trans1)
-    
     broadcaster.sendTransform([trans1, trans2, trans3,trans_aligned])
-    rospy.sleep(0.5)
+    rospy.sleep(0.1)
 
-    #get transform from aligned to device
-
-    chars_per_meter_x = server.width / resp.page_width
-    chars_per_meter_y = server.height / resp.page_height
-
-    old_x_character_id_int = 0
-    old_y_character_id_int = 0
+    pixels_per_meter_x = server.total_width_in_pixels / resp.page_width
+    pixels_per_meter_y = server.total_height_in_pixels / resp.page_height
 
     r = rospy.Rate(20)
 
+    old_x_character_id = server.width
+    old_y_character_id = server.height
+
     while not rospy.is_shutdown():
+
         device_transform = tfBuffer.lookup_transform("aligned_point",server.device_frame,rospy.Time())
 
         x_distance = device_transform.transform.translation.x
         y_distance = device_transform.transform.translation.y
 
-        x_character_id = x_distance * chars_per_meter_x
-        y_character_id = server.height - y_distance * chars_per_meter_y
+        x_pixel_coord_int = int(x_distance * pixels_per_meter_x)
+        y_pixel_coord_int = int(y_distance * pixels_per_meter_y)
 
-        x_character_id_int = int(x_character_id)
-        y_character_id_int = int(y_character_id)
-
-        if x_character_id_int >= 0 and x_character_id_int < server.width and y_character_id_int >=0 and y_character_id_int < server.height:
-
-            current_character_ascii = chr(server.ascii_page[y_character_id_int,x_character_id_int])
-            print(current_character_ascii)
-
-            publisher.publish(server.char_to_command(current_character_ascii))
-
-            if x_character_id_int != old_x_character_id_int:
-            #slight vibration
-                publisher.publish(0b01100100)
+        #check it is within page bounds
+        if x_pixel_coord_int >= 0 and x_pixel_coord_int < server.total_width_in_pixels and y_pixel_coord_int >= 0 and y_pixel_coord_int < server.total_height_in_pixels:
             
+            x_character_id, y_character_id, is_on_horizontal_spacer, is_on_vertical_spacer = server.pixel_coord_to_id_and_spacer(x_pixel_coord_int, y_pixel_coord_int)
 
-            if y_character_id_int != old_y_character_id_int:
-                #max high vibration
+            if is_on_vertical_spacer:
+                #high vibration
                 publisher.publish(0b01110100)
+            elif is_on_horizontal_spacer:
+                #slight vibration
+                publisher.publish(0b01100100)
+            else:
+                if server.sound_enabled:
+                    #check if it is on the start character
+                    if x_character_id == 0 and y_character_id == 0 and (x_character_id != old_x_character_id or y_character_id != old_y_character_id):
+                        q.put("start")
+                    elif y_character_id != old_y_character_id:
+                        q.put(str(y_character_id+1))
 
+                #it is directly over a character cell
+                current_character_ascii = chr(server.ascii_page[y_character_id,x_character_id])
+                #print(current_character_ascii)
+
+                publisher.publish(server.char_to_command(current_character_ascii))
+
+                old_x_character_id = x_character_id
+                old_y_character_id = y_character_id
+
+                
         else:
-            #out of bounds, slight vibration
+            #outside the page bounds
             publisher.publish(0b01010010)
 
 
-        
+        img = server.draw_cursor(x_pixel_coord_int, y_pixel_coord_int)
 
-        old_x_character_id_int = x_character_id_int
-        old_y_character_id_int = y_character_id_int
-
-
-
-        img = server.draw_cursor(x_character_id, y_character_id)
         cv2.imshow('Air Braille',img)
-        #cv2.waitKey(delay_in_ms)
         cv2.waitKey(1)
 
         r.sleep()
